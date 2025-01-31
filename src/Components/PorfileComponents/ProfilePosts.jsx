9f90accdb63076db9451de58cdddc5b051db25b4
Adrian-ortiz0 <dxniel7328@gmail.com>
2025-01-30 21:35:06 -0500

    feat: :sparkles: I've added the functionality to upload images on our profiles

--- src/Components/PorfileComponents/ProfilePosts.jsx
+++ src/Components/PorfileComponents/ProfilePosts.jsx
@@ -1,33 +1,67 @@
-import React, { useEffect, useState } from 'react'
-import { PostCard } from '../PublicationsComponents/PostCard'
+import React, { useEffect, useState } from 'react';
+import { PostCard } from '../PublicationsComponents/PostCard';
 import AxiosConfiguration from '../../AxiosConfiguration';
 
-export const ProfilePosts = ({usuario}) => {
-
+export const ProfilePosts = ({ usuario }) => {
   const [ownPosts, setOwnPosts] = useState([]);
 
-  const fetchOwnPosts = () => {
-    AxiosConfiguration.get(`publications/user/${usuario.id}`, {
+  const fetchImage = async (photo) => {
+    const imageUrl = `http://localhost:8083/api/publications/images/${photo}`;
+    const response = await fetch(imageUrl, {
       headers: {
-        'Authorization': `Bearer ${localStorage.getItem('authToken')}` 
+        Authorization: `Bearer ${localStorage.getItem('authToken')}`,
       },
-    }).then((response) => {
-      setOwnPosts(response.data)
-    }).catch((error) => {
-      console.error("Error fetching posts" + error)
-    })
-  }
+    });
+
+    if (!response.ok) {
+      console.error(`Error cargando imagen: ${imageUrl}`);
+      return null;
+    }
+
+    const imageBlob = await response.blob();
+    return URL.createObjectURL(imageBlob);
+  };
+
+  const fetchOwnPosts = async () => {
+
+    try {
+      const response = await AxiosConfiguration.get(`publications/user/${usuario.id}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('authToken')}`,
+        },
+      });
+
+      const postsWithImages = await Promise.all(
+        response.data.map(async (post) => ({
+          ...post,
+          photo: await fetchImage(post.photo),
+        }))
+      );
+
+      setOwnPosts(postsWithImages);
+    } catch (error) {
+      console.error('Error fetching posts:', error);
+    }
+  };
 
   useEffect(() => {
-    console.log(ownPosts)
     fetchOwnPosts();
-  }, [])
+  }, [usuario]);
 
-
-  console.log(usuario)
   return (
-    <section className='profile_posts'>
-      <PostCard />
+    <section className="profile_posts">
+      {ownPosts.length > 0 ? (
+        ownPosts.map((post) => (
+          <PostCard
+            key={post.id}
+            username={post.username}
+            img={post.photo}
+            description={post.description}
+          />
+        ))
+      ) : (
+        <p>No hay publicaciones</p>
+      )}
     </section>
-  )
-}
+  );
+};
--- src/Components/PublicationsComponents/PostCard.jsx
+++ src/Components/PublicationsComponents/PostCard.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const PostCard = ({img, description, username}) => {
   return (
-    <div>
-        
+    <div className='post_card'>
+        <img src={img} alt="" />
     </div>
   )
 }
