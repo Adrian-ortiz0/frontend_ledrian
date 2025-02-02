1b43cd0e7ffe8f3b1e3508ac1310b0c65582cb1d
DLeonardoG <darkleonardog@gmail.com>
2025-02-02 18:34:47 -0500

    style:

--- src/Components/FriendsComponents/FriendsFeed.jsx
+++ src/Components/FriendsComponents/FriendsFeed.jsx
@@ -4,8 +4,8 @@ export const FriendsFeed = ({ usuario }) => {
   const followingIds = usuario.followingIds || [];
 
   return (
-    <div className="friends_feed">
-      <div className="flex flex-col w-screen lg:flex-col lg:w-[60vw] lg:p-10 lg:left-[15vw] lg:fixed">
+    <div className="lg:friends_feed w-full h-[93vh]">
+      <div className="flex justify-center w-full lg:flex-col lg:w-[60vw] lg:p-10 lg:left-[15vw] lg:fixed">
         {followingIds.length === 0 ? (
           <p className="text-white text-center">No estás siguiendo a nadie.</p>
         ) : (
--- src/Components/MyFollowers.jsx
+++ src/Components/MyFollowers.jsx
@@ -3,16 +3,22 @@ import { useUser } from '../UserContext';
 import { AsideProfile } from './LedrianInterfazSubComponents/AsideProfile';
 import {SuggestionsSection} from './LedrianInterfazSubComponents/SuggestionsSection';
 import { FollowersFeed } from './FriendsComponents/FollowersFeed';
+import { MobileNavBar } from './Mobile/MobileAside';
+
 
 export const MyFollowers = () => {
     const { usuario } = useUser();
   return (
-    <section className='bg-gray-900  h-screen flex'>
-        <section className='flex h-screen'>
+      <>
+        <section className=' hidden lg:bg-gray-900  h-screen lg:flex h-screen'>
             <FollowersFeed usuario={usuario}/>
             <SuggestionsSection usuario={usuario}/>
+            <AsideProfile usuario={usuario} />
         </section>
-        <AsideProfile usuario={usuario} />
-    </section>
+        <section className='bg-gray-900 lg:hidden h-screen flex h-screen'>
+            <FollowersFeed usuario={usuario}/>
+            <MobileNavBar />
+        </section>
+      </>
   )
 }
