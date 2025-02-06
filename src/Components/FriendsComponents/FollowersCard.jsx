b4fb15b0d329f5e7cc706e9cd85b8b2a353086a8
Adrian-ortiz0 <dxniel7328@gmail.com>
2025-02-06 16:24:20 -0500

    feat: :ambulance: comma error

--- src/Components/FriendsComponents/FollowersCard.jsx
+++ src/Components/FriendsComponents/FollowersCard.jsx
@@ -41,7 +41,7 @@ export const FollowersCard = ({ followerId }) => {
     try {
       if (isFollowing) {
         await AxiosConfiguration.delete(
-          `/follows/unfollow?followerId=${usuario.id}&followingId=${loggedUser.id}`,,
+          `/follows/unfollow?followerId=${usuario.id}&followingId=${loggedUser.id}`,
           { headers: { Authorization: `Bearer ${authToken}` } }
         );
 
