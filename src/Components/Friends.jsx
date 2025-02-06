7b6e3b2857308f3c1bc645245d700e8f2d0ac463
Adrian-ortiz0 <dxniel7328@gmail.com>
2025-02-05 22:17:45 -0500

    feat: :sparkles: I've finished the followers section

--- src/Components/Friends.jsx
+++ src/Components/Friends.jsx
@@ -4,24 +4,21 @@ import { HeaderNav } from "./LedrianInterfazSubComponents/HeaderNav";
 import { FriendsFeed } from "./FriendsComponents/FriendsFeed";
 import { SuggestionsSection } from "./LedrianInterfazSubComponents/SuggestionsSection";
 import { useUser } from "../UserContext";
-import { MobileNavBar } from './Mobile/MobileAside';
-
-
+import { MobileNavBar } from "./Mobile/MobileAside";
 
 export const Friends = () => {
   const { usuario } = useUser();
   return (
-    <main className='bg-gray-900  h-screen flex'>
-      <section className='hidden lg:bg-gray-900  h-screen lg:flex h-screen'>
-      <AsideProfile usuario={usuario} />
+    <main className="bg-gray-900  h-screen flex">
+      <section className="hidden lg:bg-gray-900  h-screen lg:flex h-screen">
+        <AsideProfile usuario={usuario} />
         <FriendsFeed usuario={usuario} />
         <SuggestionsSection usuario={usuario} />
       </section>
 
-      <section className='flex h-screen lg:hidden'>
+      <section className="flex h-screen lg:hidden">
         <FriendsFeed usuario={usuario} />
-        <MobileNavBar/>
-        
+        <MobileNavBar />
       </section>
     </main>
   );
--- src/Components/MyFollowers.jsx
+++ src/Components/MyFollowers.jsx
@@ -1,26 +1,23 @@
-import React from 'react'
-import { useUser } from '../UserContext';
-import { AsideProfile } from './LedrianInterfazSubComponents/AsideProfile';
-import {SuggestionsSection} from './LedrianInterfazSubComponents/SuggestionsSection';
-import { FollowersFeed } from './FriendsComponents/FollowersFeed';
-import { MobileNavBar } from './Mobile/MobileAside';
-
+import React from "react";
+import { useUser } from "../UserContext";
+import { AsideProfile } from "./LedrianInterfazSubComponents/AsideProfile";
+import { SuggestionsSection } from "./LedrianInterfazSubComponents/SuggestionsSection";
+import { FollowersFeed } from "./FriendsComponents/FollowersFeed";
+import { MobileNavBar } from "./Mobile/MobileAside";
 
 export const MyFollowers = () => {
-    const { usuario } = useUser();
+  const { usuario } = useUser();
   return (
-      <>
-      <main className='bg-gray-900  h-screen flex'>
-        <section className=' hidden lg:bg-gray-900  h-screen lg:flex h-screen'>
-            <AsideProfile usuario={usuario} />
-            <FollowersFeed usuario={usuario}/>
-            <SuggestionsSection usuario={usuario}/>
-        </section>
-        <section className='bg-gray-900 lg:hidden h-screen flex h-screen'>
-            <FollowersFeed usuario={usuario}/>
-            <MobileNavBar />
-        </section>
-        </main>
-      </>
-  )
-}
+    <main className="bg-gray-900  h-screen flex">
+      <section className=" hidden lg:bg-gray-900  h-screen lg:flex h-screen">
+        <AsideProfile usuario={usuario} />
+        <FollowersFeed usuario={usuario} />
+        <SuggestionsSection usuario={usuario} />
+      </section>
+      <section className="bg-gray-900 lg:hidden h-screen flex h-screen">
+        <FollowersFeed usuario={usuario} />
+        <MobileNavBar />
+      </section>
+    </main>
+  );
+};
