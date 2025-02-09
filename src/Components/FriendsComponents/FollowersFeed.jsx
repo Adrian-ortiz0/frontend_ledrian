c71d7c1f21738b1f76c697cbd5b34781ebc0ecf9
Adrian-ortiz0 <dxniel7328@gmail.com>
2025-02-09 17:24:57 -0500

    feat: :art: Followers card and friends card have been fixed

--- src/Components/FriendsComponents/FollowersFeed.jsx
+++ src/Components/FriendsComponents/FollowersFeed.jsx
@@ -5,7 +5,7 @@ export const FollowersFeed = ({ usuario }) => {
   const followersIds = usuario?.followersIds || [];
 
   return (
-    <div className="flex justify-center w-full lg:flex-col lg:p-10">
+    <div className="flex justify-start w-full lg:flex-col lg:p-10">
       {followersIds.length === 0 ? (
         <p className="text-gray-400 text-center">Aún no tienes seguidores</p>
       ) : (
